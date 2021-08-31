ba928ca39b8e73933476467862996e839d3d8da1
Ian Huntington <ihuntington85@gmail.com>
2021-08-31 21:31:48 +0100

    chore(tailwind): update colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,16 @@ module.exports = {
         extend: {
             colors: {
                 'gt-main-opaque': 'rgba(255, 255, 255, .8)',
+                'gt-red': '#ff0000',
+                'gt-orange': '#ff9a00',
+                'gt-yellow': '#fffc00',
+                'gt-green': '#0abf01',
                 'gt-blue': '#0033ff',
-                'gt-green': '#4ce957',
-                'gt-grey': '#333333',
+                'gt-violet': '#bb79e5',
+                'gt-pink': '#ffafc8',
+                'gt-light-blue': '#75d7ef',
+                'gt-brown': '#6f4721',
+                'gt-grey': '#6f4721',
             }
         },
     },
