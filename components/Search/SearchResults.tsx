c6db38880695da79830dbabf284663f9682b2997
Ian Huntington <ihuntington85@gmail.com>
2021-09-05 18:24:48 +0100

    feat(SearchResults): add selected state

--- components/Search/SearchResults.tsx
+++ components/Search/SearchResults.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import cx from "classnames";
 
 interface IArtist {
     id: string;
@@ -11,6 +12,11 @@ interface ITrack {
     name: string;
 }
 
+interface ISearchResultItem extends ITrack {
+    onClick: (id: string) => void;
+    selected: boolean;
+}
+
 const Artists = ({ artists }: { artists: IArtist[] }) => (
     <>
         {artists.map(({ id, name }) => (
@@ -19,20 +25,33 @@ const Artists = ({ artists }: { artists: IArtist[] }) => (
     </>
 );
 
-const SearchResultItem: React.FC<Omit<ITrack, "id">> = ({ artists, name }) => (
-    <tr className="flex flex-col md:table-row w-full border-b border-gt-green hover:bg-gt-green hover:text-white">
-        <td className="text-2xl font-bold md:w-3/4 pt-2 md:pb-2 pl-2 pr-2 md:pr-0">{name}</td>
-        <td className="text-2xl w-full md:w-1/4 pb-2 md:pt-2 pr-2 pl-2 md:pl-0 md:text-right">
-            <Artists artists={artists} />
-        </td>
-    </tr>
-);
+const SearchResultItem: React.FC<ISearchResultItem> = ({ artists, name, id, onClick, selected = false }) => {
+    const classNames = cx("flex flex-col md:table-row w-full border-b border-gt-green hover:bg-gt-green hover:text-white hover:cursor-pointer", {
+        "bg-gt-green": selected,
+        "text-white": selected,
+    });
+
+    return (
+        <tr className={classNames} onClick={() => onClick(id)}>
+            <td className="text-2xl font-bold md:w-3/4 pt-2 md:pb-2 pl-2 pr-2 md:pr-0">{name}</td>
+            <td className="text-2xl w-full md:w-1/4 pb-2 md:pt-2 pr-2 pl-2 md:pl-0 md:text-right">
+                <Artists artists={artists} />
+            </td>
+        </tr>
+    );
+};
 
 export const SearchResults: React.FC<{ items: ITrack[] }> = ({ items }) => {
+    const [selected, setSelected] = useState<string | null>(null);
+
+    const handleSelect = (id: string) => {
+        setSelected(id);
+    };
+
     return (
         <table className="md:table-auto w-full">
             <tbody className="text-gt-blue flex flex-col md:table-row-group">
-            {items.map(({ artists, name, id }) => <SearchResultItem key={id} artists={artists} name={name} />)}
+            {items.map(({ artists, name, id }) => <SearchResultItem key={id} artists={artists} name={name} id={id} onClick={handleSelect} selected={selected === id} />)}
             </tbody>
         </table>
     );
