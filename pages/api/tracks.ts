f7353d6e86c74155ba9995c604027f113f49e0ba
Ian Huntington <ihuntington85@gmail.com>
2021-09-07 07:37:01 +0100

    bug(Build): remove broken tracks file

--- pages/api/tracks.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../lib/prisma";
-
-export default async function tracksHandler(req: NextApiRequest, res: NextApiResponse) {
-    const tracks = await prisma.track.findMany();
-    res.json(tracks);
-}
