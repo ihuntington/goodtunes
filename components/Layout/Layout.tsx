fd03f74681745653016b865d360968e133a8a1ab
Ian Huntington <ihuntington85@gmail.com>
2021-09-08 22:28:42 +0100

    feat(Layout): add border

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,22 @@
 export const Layout: React.FC<{ aside: React.ReactNode }> = ({ aside, children }) => {
     return (
-        <div className="container mx-auto h-full">
-            <div className="p-2 h-full">
-                <div className="md:grid md:grid-cols-12 md:gap-8 h-full">
-                    <div className="md:col-span-4 lg:col-span-3 bg-white">
-                        {aside}
-                    </div>
-                    <div className="md:col-span-8 lg:col-span-9 grid">
-                        {children}
+        <div style={{ display: "grid", gridTemplateColumns: "10px 1fr 10px", gridTemplateRows: "10px 1fr 10px" }}>
+            <div className="mx-auto h-full" style={{ gridColumnStart: "2", gridRowStart: "2" }} >
+                <div className="p-2 h-full">
+                    <div className="md:grid md:grid-cols-12 md:gap-8 h-full">
+                        <div className="md:col-span-4 lg:col-span-3 bg-white">
+                            {aside}
+                        </div>
+                        <div className="md:col-span-8 lg:col-span-9 grid">
+                            {children}
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className="bg-gt-light-blue" style={{ gridColumnStart: "1", gridColumnEnd: "3" }}></div>
+            <div className="bg-gt-orange" style={{ gridColumnStart: "3", gridRowStart: "1", gridRowEnd: "-1" }}></div>
+            <div className="bg-gt-yellow" style={{ gridColumnStart: "2", gridColumnEnd: "-1", gridRowStart: "3" }}></div>
+            <div className="bg-gt-violet" style={{ gridColumnStart: "1", gridRowStart: "2", gridRowEnd: "-1" }}></div>
         </div>
     );
 };
